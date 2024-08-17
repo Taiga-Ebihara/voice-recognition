c9848374fa3b434e4dde830e4c392228eea0e586
Taiga Ebihara <>
2024-08-17 17:55:55 +0900

    feat: マイクから音声認識できるように

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import OpenAI, { toFile } from "openai";
+import OpenAI from "openai";
 import fs from "fs";
 import path from "path";
+// @ts-ignore
+import * as recorder from "node-record-lpcm16";
 
 const client = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
+const filePath = path.resolve(__dirname, "./assets/sample.wav");
+
 async function main() {
-  const filePath = path.resolve(__dirname, "./assets/voice-sample.m4a");
   const file = fs.createReadStream(filePath);
 
   const res = await client.audio.transcriptions.create({
@@ -16,7 +19,16 @@ async function main() {
   });
 
   console.log(res);
-  fs.writeFileSync("./transcription.json", res.text);
 }
 
-main();
+const file = fs.createWriteStream(filePath, { encoding: "binary" });
+const recording = recorder.record().stream().pipe(file);
+console.log("Recording...");
+
+process.on("SIGINT", async () => {
+  recording.end();
+  console.log("Recorded");
+  console.log("Transcribing...");
+  await main();
+  process.exit();
+});
